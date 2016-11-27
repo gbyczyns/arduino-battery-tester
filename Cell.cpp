84ab370e76a37681f60c343ee67e7c187894cbe3
gbyczyns <gbyczyns@035490b4-2e51-42ce-93ce-fc3e7ed0373c>
2016-11-27 14:43:38 +0000

    Porządkowanie kodu
    git-svn-id: https://gbyczyns.svn.cloudforge.com/batterytester/trunk@3 035490b4-2e51-42ce-93ce-fc3e7ed0373c

--- Cell.cpp
+++ Cell.cpp
@@ -11,11 +11,8 @@ Cell::Cell(OutputHandler * outputHandler, byte rowNumber, byte pinVoltage, byte 
 }
 
 void Cell::printVoltage(unsigned int batVoltage) {
-    if (batVoltage < ABSOLUTE_MIN_VOLTAGE) {
-        batVoltage = 0;
-    }
     char buff[5];
-    sprintf(buff, "%4u", batVoltage);
+    sprintf(buff, "%4u", batVoltage >= ABSOLUTE_MIN_VOLTAGE ? batVoltage : 0);
     outputHandler->print(2, rowNumber, buff);
     outputHandler->print(6, rowNumber, "mV" );
 }
@@ -34,28 +31,26 @@ void Cell::printCellSummary(unsigned int batVoltage) {
 void Cell::process() {
     outputHandler->clearLine(rowNumber);
     unsigned int batVoltage = analogRead(pinVoltage) / 1023.0 * REF_VOLTAGE;
-    unsigned int fetVoltage = analogRead(pinFetVoltage) / 1023.0 * REF_VOLTAGE;
     if (battStatus == TEST_IN_PROGRESS) {
         // Calculate the time duration between the last reading (in milliseconds)
         unsigned long currentTime = millis();
-        unsigned long duration = (currentTime - prevTime);
+        unsigned long duration = currentTime - prevTime;
         prevTime = currentTime;
         if (batVoltage > ABSOLUTE_MIN_VOLTAGE) { // zabezpieczenie przed dalszym zliczaniem pojemności w przypadku nagłego odłączenia ogniwa
-          // Current through resistor is voltage across the resistor divided by load resistance. Since the voltage is in millivolts, the current will be in milliamps
-          unsigned int loadCurrent = (batVoltage - fetVoltage) / LOAD_RESISTANCE;
-          // Must divide by (60*60*1000) to convert duration in micro seconds to hours. But doing this now would cause a loss of precision, hence division by 3600 which will result in microamp hours to be summed.
-          charge += (loadCurrent * duration) / 3600;
+            unsigned int fetVoltage = analogRead(pinFetVoltage) / 1023.0 * REF_VOLTAGE;
+            // Current through resistor is voltage across the resistor divided by load resistance. Since the voltage is in millivolts, the current will be in milliamps
+            unsigned int loadCurrent = (batVoltage - fetVoltage) / LOAD_RESISTANCE;
+            // Must divide by (60*60*1000) to convert duration in micro seconds to hours. But doing this now would cause a loss of precision, hence division by 3600 which will result in microamp hours to be summed.
+            charge += (loadCurrent * duration) / 3600;
         }
         unsigned int lowerThreshold = (cellType == LI_ION_TYPE ? LIION_MIN_VOLTAGE : NIMH_MIN_VOLTAGE);
-        if (batVoltage < lowerThreshold) {
-            if (numSamplesBelowMin >= 3 ) {
-                battStatus = DONE;
-                outputHandler->beep(1800, 1000);
-            } else {
-                numSamplesBelowMin++;
-            }
-        } else {
+        if (batVoltage >= lowerThreshold) {
             numSamplesBelowMin = 0; // reset counter since this reading was good
+        } else if (numSamplesBelowMin >= 3 ) {
+            battStatus = DONE;
+            outputHandler->beep(1800, 1000);
+        } else {
+            numSamplesBelowMin++;
         }
         printCellSummary(batVoltage);
     } else if (battStatus == DETECTING_TYPE) {
@@ -109,8 +104,6 @@ void Cell::process() {
             battStatus = NOT_INSTALLED;
         }
         outputHandler->print(2, rowNumber, "nie rozpoznano    ");
-    } else {
-        outputHandler->print(2, rowNumber, "???               ");
     }
 
     if (battStatus != TEST_IN_PROGRESS) {
